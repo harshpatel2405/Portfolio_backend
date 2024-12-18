78619765437b3b3307cba66168832e5af00c825b
harshpatel2405 <harshpatel4504@gmail.com>
2024-12-18 15:56:43 +0530

    Change

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: 'https://your-vercel-app.vercel.app' }));
 
 // Connect to MongoDB
 connectDB();
