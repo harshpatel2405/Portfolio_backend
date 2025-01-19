b39229e50a4f3a3ebec09fd5d5c69082e1b9244b
harshpatel2405 <harshpatel4504@gmail.com>
2025-01-19 17:48:14 +0530

    Hello

--- models/contactModel.js
+++ models/contactModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const contactSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true,unique: true },
   message: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
 });
